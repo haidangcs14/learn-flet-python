252728889a9a810b422e34f8cc3d135a43747853
haidangcs14 <haidang9ahv1@gmail.com>
2026-03-23 17:02:57 +0700

    feat: post requests, threading, live update

--- fastapi-flet-integration/app/ui.py
+++ fastapi-flet-integration/app/ui.py
@@ -12,9 +12,29 @@ def build_ui(page: ft.Page):
     
     input_b = ft.TextField(
         label="2nd Num",
-        value="2",
+        value="20",
         width=200
     )
+
+    def call_api(_):
+        num1 = int(input_a.value)
+        num2 = int(input_b.value)
+
+        main_message.value = "Calculating..."
+        page.update()
+
+        def make_request():
+            response = requests.post(
+                                    "http://localhost:8550/api/add", 
+                                    json={"a": num1, "b": num2},
+                                    timeout=10,
+                                )
+            data = response.json()
+            main_message.value = f"Result: {data['result']}"
+
+            page.update()
+        
+        page.run_thread(make_request)
 
     def get_health():
         res = requests.get("http://localhost:8550/api/health")
@@ -26,12 +46,14 @@ def build_ui(page: ft.Page):
     main_message = ft.Text("Enter 2 numbers", size=40)
     
     health_button = ft.Button("Get Health", on_click=lambda e: page.run_thread(get_health))
+    calc_button = ft.Button("Calculation", on_click=call_api)
 
     page.add(
-        main_message, 
         ft.Row([
             input_a,
             input_b,
+            calc_button,
         ]),
+        main_message, 
         health_button,
     )
